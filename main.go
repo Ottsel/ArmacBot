2e2c9c2d15561f6b7095469640dca9118098724b
ShadowJonathan <jonathandejong02@gmail.com>
2017-01-12 15:34:43 +0100

    adding new bug-fix (#2)
    * rearranging the vars and funcs of main
    
    * added bug-fix

--- main.go
+++ main.go
@@ -12,6 +12,26 @@ import (
 	"time"
 )
 
+// main function
+func main() {
+	dg, e := discordgo.New("Bot " + cfg.BotToken)
+	if e != nil {
+		log.Println("Error:", e)
+		return
+	}
+	dg.AddHandlerOnce(ready)
+	dg.AddHandler(messageCreate)
+	dg.AddHandler(presenceUpdate)
+	dg.AddHandler(voiceState)
+	if e := dg.Open(); e != nil {
+		log.Println("Error:", e)
+		return
+	}
+	<-make(chan struct{})
+	return
+}
+
+// vars and declarations here
 var (
 	sounds      []os.FileInfo
 	adminRoleID string
@@ -29,6 +49,8 @@ type Configuration struct {
 }
 
 var cfg Configuration
+
+// functions after here
 
 func init() {
 	if _, e := os.Stat("sounds"); os.IsNotExist(e) {
@@ -61,23 +83,6 @@ func init() {
 			return
 		}
 	}
-}
-func main() {
-	dg, e := discordgo.New("Bot " + cfg.BotToken)
-	if e != nil {
-		log.Println("Error:", e)
-		return
-	}
-	dg.AddHandlerOnce(ready)
-	dg.AddHandler(messageCreate)
-	dg.AddHandler(presenceUpdate)
-	dg.AddHandler(voiceState)
-	if e := dg.Open(); e != nil {
-		log.Println("Error:", e)
-		return
-	}
-	<-make(chan struct{})
-	return
 }
 func ready(s *discordgo.Session, event *discordgo.Event) {
 	go func() {
