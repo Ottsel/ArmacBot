531504b3c2e76af0a2c45ab13418e5af566a4bd1
Unknown <nicholas.hunter@bellevuecollege.edu>
2017-04-10 15:32:28 -0700

    Removed many features
    Made a smaller, lighter bot for role management only.

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"C"
 	"flag"
-	"github.com/bwmarrin/discordgo"
-	"io/ioutil"
 	"log"
-	"os"
-	"strings"
-	"time"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 var (
-	sounds      []os.FileInfo
-	adminRoleID string
 	botID       string
+	adminRoleID string
 )
 
 func main() {
@@ -27,9 +22,6 @@ func main() {
 	if err(e, "") {
 		return
 	}
-	dg.AddHandler(messageCreate)
-	dg.AddHandler(presenceUpdate)
-	dg.AddHandler(voiceState)
 	dg.AddHandler(onGuildCreate)
 	e = dg.Open()
 	if err(e, "") {
@@ -42,169 +34,103 @@ func onGuildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
 	if event.Guild.Unavailable == true {
 		return
 	}
-	if !isConfigured(event.Guild) {
-		s.ChannelMessageSend(event.Guild.ID, "Not configured. Type `"+cfg.AdminCommandKey+"config` in the channel you would like to keep commands in.")
-	}
 	roleFix(s, event.Guild)
 }
-func messageCreate(s *discordgo.Session, mc *discordgo.MessageCreate) {
-	g := getGuild(s, mc.ChannelID)
-	config(g)
-
-	/*
-	 *	Admin commands
-	 */
-
-	//Configure via Discord
-	if strings.HasPrefix(mc.Content, cfg.AdminCommandKey+"config") {
-		if !isConfigured(g) {
-			if authenticate(s, g.ID, mc.Author) {
-				m, e := s.ChannelMessageSend(mc.ChannelID, "Listing available sounds and pinning message...")
-				if err(e, "Couldn't post message: "+m.Content) {
-					return
-				}
-				writeConfig(g, mc.ChannelID, m.ID)
-				listSounds(s, g)
-				e = s.ChannelMessagePin(m.ChannelID, m.ID)
-				if err(e, "") {
-					return
-				}
+func authenticate(s *discordgo.Session, g string, u *discordgo.User) bool {
+	user, e := s.GuildMember(g, u.ID)
+	if err(e, "") {
+		return false
+	}
+	roles, e := s.GuildRoles(g)
+	if err(e, "") {
+		return false
+	}
+	for _, ar := range roles {
+		if ar.Permissions == 8 {
+			adminRoleID = ar.ID
+		}
+	}
+	if adminRoleID != "" {
+		for _, r := range user.Roles {
+			if r == adminRoleID {
+				return true
 			}
 		}
+	} else {
+		log.Println("Make sure admins only have the permission \"Administrator,\" they override other permissions anyway. ;)")
+		return false
 	}
+	return false
+}
 
-	//Corrects the roles of the current guild
-	if strings.HasPrefix(mc.Content, cfg.AdminCommandKey+"fixroles ") {
-		if authenticate(s, g.ID, mc.Author) {
-			roleFix(s, g)
-		} else {
-			log.Println("Failed to authenticate user:", mc.Author.Username)
-		}
-	}
-	//Deletes messages up to a specified message
-	if strings.HasPrefix(mc.Content, cfg.AdminCommandKey+"forget ") {
-		if authenticate(s, g.ID, mc.Author) {
-			log.Println("Authenticated user:", mc.Author.Username)
-			forget(s, mc)
-		} else {
-			log.Println("Failed to authenticate user:", mc.Author.Username)
-		}
-	}
-
-	/*
-	 *	Soundboard commands
-	 */
-
-	if strings.HasPrefix(mc.Content, cfg.SoundboardCommandKey) {
-		channel, e := s.Channel(mc.ChannelID)
-		if err(e, "") {
-			return
-		}
-		if channel.ID != cfg.CommandChannelID {
-			e := s.ChannelMessageDelete(channel.ID, mc.ID)
-			if err(e, "Couldn't delete message:"+mc.Content) {
-				return
-			}
-		}
-		//Stop the player
-		if mc.Content == cfg.SoundboardCommandKey+"stop" {
-			KillPlayer()
-			return
-		}
-		//Play a requested sound
-		soundString := strings.Replace(mc.Content, cfg.SoundboardCommandKey, "", 1) + ".mp3"
-		sounds, _ = ioutil.ReadDir(soundDir)
-		for _, f := range sounds {
-			if f.Name() == soundString {
-				playSound(s, g, mc.Author, soundString)
-			}
-		}
-	}
-	//Post a list of admin commands
-	if strings.HasPrefix(mc.Content, cfg.AdminCommandKey+"help") {
-		if authenticate(s, g.ID, mc.Author) {
-			help(s, mc)
-		}
+func presenceUpdate(s *discordgo.Session, p *discordgo.PresenceUpdate) {
+	correctRoles(s, p.GuildID, &p.Presence)
+}
+func roleFix(s *discordgo.Session, g *discordgo.Guild) {
+	log.Println("Correcting Roles in guild: ", g.Name)
+	botID = s.State.User.ID
+	for _, p := range g.Presences {
+		correctRoles(s, g.ID, p)
 	}
 }
-func voiceState(s *discordgo.Session, vsu *discordgo.VoiceStateUpdate) {
-	guild, e := s.Guild(vsu.GuildID)
+func correctRoles(s *discordgo.Session, g string, p *discordgo.Presence) {
+	if authenticate(s, g, p.User) {
+		log.Println("Admin, cannot modify role")
+		return
+	}
+	var role string
+
+	guild, e := s.Guild(g)
 	if err(e, "") {
 		return
 	}
-	config(guild)
-	user, e := s.GuildMember(vsu.GuildID, vsu.UserID)
+	guildRoles, e := s.GuildRoles(g)
 	if err(e, "") {
 		return
 	}
 
-	file := (strings.ToLower(user.User.Username) + ".mp3")
-	if _, e := os.Stat(soundDir + "/" + file); os.IsNotExist(e) {
-		log.Println("No entrance for user: ", user.User.Username)
-	} else {
-		guild, e := s.Guild(vsu.GuildID)
-		if err(e, "") {
-			return
-		}
-		var botVC string
-		for _, v := range guild.VoiceStates {
-			if v.UserID == botID {
-				botVC = v.ChannelID
-			}
-		}
-		for _, v := range guild.VoiceStates {
-			if v.UserID == user.User.ID {
-				if v.ChannelID != botVC && v.ChannelID != "" {
-					playSound(s, guild, user.User, file)
+	var updatedRoles []string
+
+	for _, gr := range guildRoles {
+		if p.Game != nil {
+			if gr.Name == p.Game.Name {
+				role = gr.Name
+				updatedRoles = append(updatedRoles, gr.ID)
+			} else {
+				if gr.Name == "Other Games" {
+					role = "Other Games"
+					updatedRoles = append(updatedRoles, gr.ID)
 				} else {
-					return
+					updatedRoles = append(updatedRoles, "1")
+					role = guild.Name
+
+					log.Println("No role by name \"Other Games\", putting user in default role")
 				}
+			}
+		} else {
+			updatedRoles = append(updatedRoles, "1")
+		}
+		if p.User.Bot {
+			if gr.Name == "Bots" {
+				role = "Bot"
+				updatedRoles = append(updatedRoles, gr.ID)
 			}
 		}
 	}
-}
-func playSound(s *discordgo.Session, g *discordgo.Guild, user *discordgo.User, file string) {
-	config(g)
-	var userVC string
-	for _, v := range g.VoiceStates {
-		if v.UserID == user.ID {
-			userVC = v.ChannelID
-		}
-	}
-	vc, e := s.ChannelVoiceJoin(g.ID, userVC, false, false)
+	log.Println("Changing user role to:", role)
+	e = s.GuildMemberEdit(g, p.User.ID, updatedRoles)
 	if err(e, "") {
 		return
 	}
-	go func() {
-		time.Sleep(time.Millisecond * 200)
-		g, e = s.Guild(g.ID)
-		for _, v := range g.VoiceStates {
-			if v.UserID == s.State.User.ID {
-				if v.ChannelID == userVC {
-					listSounds(s, g)
-					log.Println("Attempting to play audio file \"" + soundDir + "/" + file + "\" for user: " + user.Username)
-					KillPlayer()
-					time.Sleep(time.Millisecond * 200)
-					PlayAudioFile(vc, (soundDir + "/" + file))
-				}
-			}
-		}
-	}()
 }
-func listSounds(s *discordgo.Session, g *discordgo.Guild) {
-	config(g)
-	var sounds string
-	files, _ := ioutil.ReadDir(soundDir)
-	for _, f := range files {
-		sounds += cfg.SoundboardCommandKey + strings.Replace(f.Name(), ".mp3", "", 1) + "\n"
-	}
-	if sounds == "" {
-		sounds = "(No mp3 files in directory: " + soundDir + ")"
-	}
-	m, e := s.ChannelMessageEdit(cfg.CommandChannelID, cfg.SoundboardMessageID, sounds)
-	if err(e, "Couldn't edit soundboard message with ID: "+m.ID) {
-		return
+func err(e error, c string) bool {
+	if e != nil {
+		if c != "" {
+			log.Println(c)
+		}
+		log.Println("Error:", e)
+		return true
 	} else {
-		log.Println("Sounds Updated")
+		return false
 	}
 }
