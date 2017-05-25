5885ef9cbdb37c48b8b269c7470e12f5fec439af
Unknown <nicholas.hunter@bellevuecollege.edu>
2017-05-24 18:54:50 -0700

    Dank roles
    you heard it

--- main.go
+++ main.go
@@ -3,13 +3,9 @@ package main
 import (
 	"flag"
 	"log"
+	"strconv"
 
 	"github.com/bwmarrin/discordgo"
-)
-
-var (
-	botID       string
-	adminRoleID string
 )
 
 func main() {
@@ -23,6 +19,7 @@ func main() {
 		return
 	}
 	dg.AddHandler(onGuildCreate)
+	dg.AddHandler(presenceUpdate)
 	e = dg.Open()
 	if err(e, "") {
 		return
@@ -33,94 +30,78 @@ func main() {
 func onGuildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
 	if event.Guild.Unavailable == true {
 		return
-	}
-	roleFix(s, event.Guild)
-}
-func authenticate(s *discordgo.Session, g string, u *discordgo.User) bool {
-	user, e := s.GuildMember(g, u.ID)
-	if err(e, "") {
-		return false
-	}
-	roles, e := s.GuildRoles(g)
-	if err(e, "") {
-		return false
-	}
-	for _, ar := range roles {
-		if ar.Permissions == 8 {
-			adminRoleID = ar.ID
-		}
-	}
-	if adminRoleID != "" {
-		for _, r := range user.Roles {
-			if r == adminRoleID {
-				return true
-			}
-		}
 	} else {
-		log.Println("Make sure admins only have the permission \"Administrator,\" they override other permissions anyway. ;)")
-		return false
+		roleFix(s, event.Guild)
 	}
-	return false
 }
 
 func presenceUpdate(s *discordgo.Session, p *discordgo.PresenceUpdate) {
 	correctRoles(s, p.GuildID, &p.Presence)
 }
 func roleFix(s *discordgo.Session, g *discordgo.Guild) {
-	log.Println("Correcting Roles in guild: ", g.Name)
-	botID = s.State.User.ID
+	guildRoles, e := s.GuildRoles(g.ID)
+	if err(e, "") {
+		return
+	}
+
+	log.Println("Correcting Roles in guild:", g.Name+"\n")
+
+	log.Println(strconv.Itoa(len(g.Presences)) + " users online")
+	log.Println(strconv.Itoa(len(guildRoles)) + " roles available: \n")
+	for _, r := range guildRoles {
+		log.Println(r.Name, " - ", r.ID)
+	}
 	for _, p := range g.Presences {
 		correctRoles(s, g.ID, p)
 	}
 }
 func correctRoles(s *discordgo.Session, g string, p *discordgo.Presence) {
-	if authenticate(s, g, p.User) {
-		log.Println("Admin, cannot modify role")
-		return
-	}
-	var role string
+	if !p.User.Bot {
 
-	guild, e := s.Guild(g)
-	if err(e, "") {
-		return
-	}
-	guildRoles, e := s.GuildRoles(g)
-	if err(e, "") {
-		return
-	}
+		guild, e := s.Guild(g)
+		if err(e, "") {
+			return
+		}
+		guildRoles, e := s.GuildRoles(g)
+		if err(e, "") {
+			return
+		}
 
-	var updatedRoles []string
+		var updatedRoles []string
+		var role string
 
-	for _, gr := range guildRoles {
-		if p.Game != nil {
-			if gr.Name == p.Game.Name {
-				role = gr.Name
-				updatedRoles = append(updatedRoles, gr.ID)
-			} else {
-				if gr.Name == "Other Games" {
-					role = "Other Games"
+		if p.Game == nil {
+			updatedRoles = append(updatedRoles, guild.ID)
+			role = guild.Name
+		} else {
+			for _, gr := range guildRoles {
+				if gr.Name == p.Game.Name {
+					role = gr.Name
 					updatedRoles = append(updatedRoles, gr.ID)
-				} else {
-					updatedRoles = append(updatedRoles, "1")
-					role = guild.Name
-
-					log.Println("No role by name \"Other Games\", putting user in default role")
 				}
 			}
-		} else {
-			updatedRoles = append(updatedRoles, "1")
-		}
-		if p.User.Bot {
-			if gr.Name == "Bots" {
-				role = "Bot"
-				updatedRoles = append(updatedRoles, gr.ID)
+			if role == "" {
+				for _, gr := range guildRoles {
+					if gr.Name == "Other Games" {
+						role = "Other Games"
+						updatedRoles = append(updatedRoles, gr.ID)
+					}
+				}
+				if role == "" {
+					updatedRoles = append(updatedRoles, guild.ID)
+					role = guild.Name
+				}
 			}
 		}
-	}
-	log.Println("Changing user role to:", role)
-	e = s.GuildMemberEdit(g, p.User.ID, updatedRoles)
-	if err(e, "") {
-		return
+		if role == guild.Name && p.Game != nil {
+			log.Println("No role by name \"Other Games\", putting user in default role")
+		} else {
+			log.Println("Changing "+p.User.Username+"'s role to:", role)
+		}
+		e = s.GuildMemberEdit(g, p.User.ID, updatedRoles)
+		if err(e, "") {
+			return
+		}
 	}
 }
 func err(e error, c string) bool {
